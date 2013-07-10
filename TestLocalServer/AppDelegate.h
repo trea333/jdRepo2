cae1644d93ae11d17fa37f6a838ce6b4063f04e7
USI <usi@usi-nb-1091.local>
2013-07-10 11:32:13 -0400

    Initial Commit

---
+++ TestLocalServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestLocalServer
+//
+//  Created by USI on 10/07/2013.
+//  Copyright (c) 2013 USI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
